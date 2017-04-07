1cd7826ff577412eac00b7bf2d18f9eef2c16a1d
trieu <tantrieuf31@gmail.com>
2017-04-07 17:49:58 +0700

    update masthead mobile android

--- sample_app/src/main/java/net/adsplay/scalablevideo/sample/MainActivity.java
+++ sample_app/src/main/java/net/adsplay/scalablevideo/sample/MainActivity.java
@@ -1,8 +1,17 @@
 package net.adsplay.scalablevideo.sample;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,6 +19,9 @@ import android.webkit.WebViewClient;
 import net.adsplay.common.AdsPlayCallback;
 import net.adsplay.holder.AdsPlayImageBannerHolder;
 import net.adsplay.holder.AdsPlayVideoInfeedHolder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -41,13 +53,75 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("AdsPlay", "------->  loaded PlacementId: " + infeedPlacementId);
             }
         };
-        AdsPlayVideoInfeedHolder infeedHolder = (AdsPlayVideoInfeedHolder) findViewById(R.id.masterhead_view);
-        infeedHolder.loadAdData(this, infeedPlacementId,  infeedCallback);
+        //AdsPlayVideoInfeedHolder infeedHolder = (AdsPlayVideoInfeedHolder) findViewById(R.id.masterhead_view);
+        //infeedHolder.loadAdData(this, infeedPlacementId,  infeedCallback);
 
-        WebView myWebView = (WebView) findViewById(R.id.masthead_webview);
+        final WebView myWebView = (WebView) findViewById(R.id.masthead_webview);
+        myWebView.setBackgroundColor(Color.TRANSPARENT);
+        myWebView.setVerticalScrollBarEnabled(false);
+        myWebView.setHorizontalScrollBarEnabled(false);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("http://richmedia.adsplay.net/masthead-mobile");
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setGeolocationEnabled(true);
+
+
+//        myWebView.setWebChromeClient(new WebChromeClient() {
+//            @Override
+//            public boolean onConsoleMessage(ConsoleMessage cs) {
+//                Log.i("AdsPlaySDK", cs.message() + " -- From line "
+//                        + cs.lineNumber() + " of "
+//                        + cs.sourceId());
+//                return true;
+//            }
+//        });
+        WebViewClient webViewClient= new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView  view, String  url){
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(i);
+                Log.d("adsplay",url);
+                myWebView.setVisibility(View.GONE);
+                return true;
+            }
+
+            @Override
+            public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                //myWebView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                myWebView.loadUrl("javascript:window.HtmlViewer.check" +
+                        "('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
+            }
+        };
+
+        Map<String, String> extraHeaders = new HashMap<>();
+        extraHeaders.put("Referer", "https://fptplay.net");
+        myWebView.addJavascriptInterface(new MyJavaScriptInterface(myWebView), "HtmlViewer");
+        myWebView.setWebViewClient(webViewClient);
+        myWebView.loadUrl("http://st.adsplay.net/js/masthead/masthead.html",extraHeaders);
+    }
+
+    static final class MyJavaScriptInterface {
+
+        private WebView myWebView;
+
+        MyJavaScriptInterface(WebView myWebView) {
+            this.myWebView = myWebView;
+        }
+
+        public void check(String html) {
+            Log.d("adsplay",html);
+            if(!html.contains("html")){
+               // myWebView.setVisibility(View.GONE);
+            }
+        }
+
     }
 }
